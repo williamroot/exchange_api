d061a0d92736aa4d9d3e6b1e2f4acccdd2c58008
William Souza <williamalvesroot@gmail.com>
2016-01-05 07:42:05 +0000

    add admin

--- src/api/admin.py
+++ src/api/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from models import Currency, Exchange
 
-# Register your models here.
+class CurrencyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'iso_code',)
+    model = Currency
+
+class ExchangeAdmin(admin.ModelAdmin):
+    list_display = ('source', 'target', 'value', 'created',)
+    model = Exchange
+
+admin.site.register(Currency, CurrencyAdmin)
+admin.site.register(Exchange, ExchangeAdmin)
