c4224147c0f740de47e15e26480ed5b920d4b037
William Souza <williamalvesroot@gmail.com>
2016-01-13 06:25:28 +0000

    add doc

--- src/exchange_api/urls.py
+++ src/exchange_api/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^', include('api.urls', namespace='api')),
-    url(
-        r'^api-auth/', include(
-            'rest_framework.urls', namespace='rest_framework'
-        )
-    )
+    url(r'^', include('rest_framework.urls', namespace='rest_framework_urls')),
 ]
